db25f80848c3c65d3ac71a0fa5858837f17b4ca6
marcobuontempo <marco.buon36@gmail.com>
2023-06-07 00:06:15 +1000

    Fix imports

--- __tests__/board.test.ts
+++ __tests__/board.test.ts
@@ -1,13 +1,13 @@
-import * as board from "../src/board";
+import { SQ } from "../src/board";
 
 describe("piece value tests", () => {
     test("white unmoved pawn should equal 1", () => {
-        expect(board.P | board.w).toStrictEqual(1);
+        expect(SQ.P | SQ.w).toStrictEqual(1);
     });
     test("white moved pawn should equal 65", () => {
-        expect(board.P | board.w | board.m).toStrictEqual(65);
+        expect(SQ.P | SQ.w | SQ.m).toStrictEqual(65);
     });
     test("white moved knight should equal 2", () => {
-        expect(board.N | board.w).toStrictEqual(2);
+        expect(SQ.N | SQ.w).toStrictEqual(2);
     });
 });

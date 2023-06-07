57cba85c3419ce62b97ab5553a511e61291291c0
marcobuontempo <marco.buon36@gmail.com>
2023-06-08 00:53:16 +1000

    Structure board and engine logic into separate classes

--- src/board.ts
+++ src/board.ts
@@ -1,224 +1,184 @@
-/**
- * SQUARE ENCODINGS
- * binary representation used to encode piece information
- * 
- * usage:
- *  white pawn (moved)                  ->  w|P|m  -> 65
- *  black king (unmoved, can castle)    ->  b|K|c  -> 54
- *  empty square                        ->         -> 0
- */
-export const SQ = {
-  /* PIECES (bits 0-2)*/
-  P: 0b0000_0001,    //| pawn:               1   
-  N: 0b0000_0010,    //| knight:             2
-  B: 0b0000_0011,    //| bishop:             3
-  R: 0b0000_0100,    //| rook:               4
-  Q: 0b0000_0101,    //| queen:              5
-  K: 0b0000_0110,    //| king:               6
+export default class ChessBoard {
+  board: Int8Array;
 
-  /* FLAGS (bits 4-6) */
-  w: 0b0000_0000,    //| white:              0
-  b: 0b0001_0000,    //| black:              16
-  c: 0b0010_0000,    //| can castle (king):  32
-  m: 0b0100_0000,    //| piece has moved:    64
-};
-
-/** REVERSE SQ LOOKUP */
-export const SQ_R = {
-  0b0000_0001: "P",
-  0b0000_0010: "N",
-  0b0000_0011: "B",
-  0b0000_0100: "R",
-  0b0000_0101: "Q",
-  0b0000_0110: "K",
-  0b0000_0000: "w",
-  0b0001_0000: "b",
-  0b0010_0000: "c",
-  0b0100_0000: "m",
-};
-
-/**
-* MOVE DIRECTIONS
-* index matches piece encoding
-* e.g. king = 6 = MOVES_LIST[6]
-*/
-export const NESW = {
-  N: -10,
-  S: 10,
-  E: 1,
-  W: -1,
-  NE: -9,
-  NW: -11,
-  SE: 11,
-  SW: 9,
-};
-
-
-/**
-* THE POSSIBLE DIRECTIONS THAT EACH PIECE CAN GO IN 1-STEP
-* can be accessed using the the piece value (excl. black pawn). i.e. knight = 2 = MOVES_LIST[2]
-*/
-export const MOVES_LIST = [
-  /* black pawn */ [(NESW.S), (NESW.S + NESW.S), (NESW.SE), (NESW.SW)],
-  /* white pawn */ [(NESW.N), (NESW.N + NESW.N), (NESW.NE), (NESW.NW)],
-  /*     knight */ [(NESW.N + NESW.NE), (NESW.N + NESW.NW), (NESW.E + NESW.NE), (NESW.E + NESW.SE), (NESW.S + NESW.SE), (NESW.S + NESW.SW), (NESW.W + NESW.NW), (NESW.W + NESW.SW)],
-  /*     bishop */ [(NESW.NE), (NESW.NW), (NESW.SE), (NESW.SW)],
-  /*       rook */ [(NESW.N), (NESW.S), (NESW.E), (NESW.W)],
-  /*      queen */ [(NESW.N), (NESW.S), (NESW.E), (NESW.W), (NESW.NE), (NESW.NW), (NESW.SE), (NESW.SW)],
-  /*       king */ [(NESW.N), (NESW.S), (NESW.E), (NESW.W), (NESW.NE), (NESW.NW), (NESW.SE), (NESW.SW)]
-];
-
-
-/**
-* WHETHER A PIECE IS A SLIDER OR NOT (I.E. WHETHER IT CAN GO MORE THAN 1-STEP)
-* can be accessed using the the piece value (excl. black pawn). i.e. knight = 2 = MOVES_LIST[2]
-*/
-export const SLIDERS = [
-  /* black pawn */ false,
-  /* white pawn */ false,
-  /*     knight */ false,
-  /*     bishop */ true,
-  /*       rook */ true,
-  /*      queen */ true,
-  /*       king */ false
-];
-
-
-/**
- *  MAILBOX BOARD STRUCTURES
- * 
- * [64]  -> a lookup table to get the index value of the outer board
- * 
- * [120] -> used for efficient move generation
- *       -> actual board embedded in centre
- *       -> board edges padded with '-1' values (+1 col E&W, +2 rows N&S)
- *  
- * Usage
- * -> A1 = mailbox64[56] = mailbox120[91]
- * -> H8 = mailbox64[7]  = mailbox120[28]
- * 
- *     -- OUTER BOARD INDEXES [120] --              -- ACTUAL BOARD INDEXES [64] --
- *   0   1   2   3   4   5   6   7   8   9
- *  10  11  12  13  14  15  16  17  18  19
- *  20  21  22  23  24  25  26  27  28  29         8)  21  22  23  24  25  26  27  28
- *  30  31  32  33  34  35  36  37  38  39         7)  31  32  33  34  35  36  37  38
- *  40  41  42  43  44  45  46  47  48  49         6)  41  42  43  44  45  46  47  48
- *  50  51  52  53  54  55  56  57  58  59    =>   5)  51  52  53  54  55  56  57  58
- *  60  61  62  63  64  65  66  67  68  69    <=   4)  61  62  63  64  65  66  67  68
- *  70  71  72  73  74  75  76  77  78  79         3)  71  72  73  74  75  76  77  78
- *  80  81  82  83  84  85  86  87  88  89         2)  81  82  83  84  85  86  87  88
- *  90  91  92  93  94  95  96  97  98  99         1)  91  92  93  94  95  96  97  98
- * 100 101 102 103 104 105 106 107 108 109             A   B   C   D   E   F   G   H
- * 110 111 112 113 114 115 116 117 118 119
- * 
- */
-export const mailbox64 = new Int8Array([
-  21, 22, 23, 24, 25, 26, 27, 28,
-  31, 32, 33, 34, 35, 36, 37, 38,
-  41, 42, 43, 44, 45, 46, 47, 48,
-  51, 52, 53, 54, 55, 56, 57, 58,
-  61, 62, 63, 64, 65, 66, 67, 68,
-  71, 72, 73, 74, 75, 76, 77, 78,
-  81, 82, 83, 84, 85, 86, 87, 88,
-  91, 92, 93, 94, 95, 96, 97, 98
-]);
-export const mailbox120 = new Int8Array([
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-  -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-  -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-  -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-  -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-  -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-  -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-  -1,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-]);
-
-
-/**
- * PADS A []64 BOARD WITH -1'S TO MAKE []120
- */
-export function padBoard(inpBoard: Int8Array) {
-  const outBoard = new Int8Array([...mailbox120]);
-  mailbox64.forEach((mi, i) => {
-    outBoard[mi] = inpBoard[i];
-  });
-  return outBoard;
-}
-
-
-/** CHARACTER MAPPINGS */
-export const PIECE_CH = {
-  0b0000_0000: ".",
-  0b0000_0001: "P",
-  0b0000_0010: "N",
-  0b0000_0011: "B",
-  0b0000_0100: "R",
-  0b0000_0101: "Q",
-  0b0000_0110: "K",
-  0b0001_0001: "p",
-  0b0001_0010: "n",
-  0b0001_0011: "b",
-  0b0001_0100: "r",
-  0b0001_0101: "q",
-  0b0001_0110: "k",
-};
-
-/** UNICODE MAPPINGS */
-export const PIECE_UTF = {
-  0b0000_0000: ".",
-  0b0000_0001: "♙",
-  0b0000_0010: "♘",
-  0b0000_0011: "♗",
-  0b0000_0100: "♖",
-  0b0000_0101: "♕",
-  0b0000_0110: "♔",
-  0b0001_0001: "♟",
-  0b0001_0010: "♞",
-  0b0001_0011: "♝",
-  0b0001_0100: "♜",
-  0b0001_0101: "♛",
-  0b0001_0110: "♚",
-};
-
-
-/**
- * PRINTS BOARD TO TERMINAL
- * can use decimal, character, or unicode notation
- */
-export function printBoard(board120: Int8Array, pieceSymbol: "decimal" | "character" | "unicode" = "unicode") {
-  process.stdout.write(" +---------------------------+\n");
-  process.stdout.write("+-----------------------------+\n| |");
-
-  for (let i = 21; i <= 98; i++) {
-    if (i % 10 === 0) { process.stdout.write("| |"); continue; }
-    if (i % 10 === 9) { process.stdout.write(" | |\n"); continue; }
-
-    let square = "";
-    const piece = board120[i] & 0b0001_0111;  // get *only* the piece and colour value for lookup
-
-    switch (pieceSymbol) {
-    case "decimal":
-      square = String(board120[i]);
-      break;
-    case "character":
-      square = ` ${PIECE_CH[piece as keyof typeof PIECE_CH]} `;
-      break;
-    case "unicode":
-      square = ` ${PIECE_UTF[piece as keyof typeof PIECE_UTF]} `;
-      break;
-    default:
-      square = "ERR";
-      break;
-    }
-
-    square = square.padStart(3);
-    process.stdout.write(square);
+  constructor(board8x8: Int8Array) {
+    this.board = ChessBoard.padBoard(board8x8);
   }
 
-  process.stdout.write(" | |\n+-----------------------------+\n");
-  process.stdout.write(" +---------------------------+\n");
+  /**
+   * SQUARE INFORMATION ENCODINGS
+   * binary representation to encode piece information
+   * 
+   * bit 1-3: piece information
+   * bit 4: unused
+   * bit 5: colour (0 white, 1 black)
+   * bit 6: king castle rights
+   * bit 7: piece has moved
+   * bit 8: unused
+   */
+  static SQ = {
+    P: 0b0000_0001,    //| pawn:               1   
+    N: 0b0000_0010,    //| knight:             2
+    B: 0b0000_0011,    //| bishop:             3
+    R: 0b0000_0100,    //| rook:               4
+    Q: 0b0000_0101,    //| queen:              5
+    K: 0b0000_0110,    //| king:               6
+    w: 0b0000_0000,    //| white:              0
+    b: 0b0001_0000,    //| black:              16
+    c: 0b0010_0000,    //| can castle (king):  32
+    m: 0b0100_0000,    //| piece has moved:    64
+  };
+
+  /** REVERSE SQ LOOKUP */
+  static SQ_R = {
+    0b0000_0001: "P",
+    0b0000_0010: "N",
+    0b0000_0011: "B",
+    0b0000_0100: "R",
+    0b0000_0101: "Q",
+    0b0000_0110: "K",
+    0b0000_0000: "w",
+    0b0001_0000: "b",
+    0b0010_0000: "c",
+    0b0100_0000: "m",
+  };
+
+  /** PIECE -> CHARACTER MAPPINGS */
+  static PIECE_CH = {
+    0b0000_0000: ".",
+    0b0000_0001: "P",
+    0b0000_0010: "N",
+    0b0000_0011: "B",
+    0b0000_0100: "R",
+    0b0000_0101: "Q",
+    0b0000_0110: "K",
+    0b0001_0001: "p",
+    0b0001_0010: "n",
+    0b0001_0011: "b",
+    0b0001_0100: "r",
+    0b0001_0101: "q",
+    0b0001_0110: "k",
+  };
+
+  /** PIECE -> UNICODE MAPPINGS */
+  static PIECE_UTF = {
+    0b0000_0000: ".",
+    0b0000_0001: "♙",
+    0b0000_0010: "♘",
+    0b0000_0011: "♗",
+    0b0000_0100: "♖",
+    0b0000_0101: "♕",
+    0b0000_0110: "♔",
+    0b0001_0001: "♟",
+    0b0001_0010: "♞",
+    0b0001_0011: "♝",
+    0b0001_0100: "♜",
+    0b0001_0101: "♛",
+    0b0001_0110: "♚",
+  };
+
+  /**
+  *  MAILBOX BOARD STRUCTURES
+  * 
+  * [64]  -> a lookup table to get the index value of the outer board
+  * 
+  * [120] -> used for efficient move generation
+  *       -> actual 8x8 board embedded in centre
+  *       -> board edges padded with '-1' values (+1 col E&W, +2 rows N&S)
+  *  
+  * Example:
+  * -> A1 = mailbox64[56] = mailbox120[91]
+  * -> H8 = mailbox64[7]  = mailbox120[28]
+  * 
+  *     -- OUTER BOARD INDEXES [120] --              -- ACTUAL BOARD INDEXES [64] --
+  *   0   1   2   3   4   5   6   7   8   9
+  *  10  11  12  13  14  15  16  17  18  19
+  *  20  21  22  23  24  25  26  27  28  29         8)  21  22  23  24  25  26  27  28
+  *  30  31  32  33  34  35  36  37  38  39         7)  31  32  33  34  35  36  37  38
+  *  40  41  42  43  44  45  46  47  48  49         6)  41  42  43  44  45  46  47  48
+  *  50  51  52  53  54  55  56  57  58  59    =>   5)  51  52  53  54  55  56  57  58
+  *  60  61  62  63  64  65  66  67  68  69    <=   4)  61  62  63  64  65  66  67  68
+  *  70  71  72  73  74  75  76  77  78  79         3)  71  72  73  74  75  76  77  78
+  *  80  81  82  83  84  85  86  87  88  89         2)  81  82  83  84  85  86  87  88
+  *  90  91  92  93  94  95  96  97  98  99         1)  91  92  93  94  95  96  97  98
+  * 100 101 102 103 104 105 106 107 108 109             A   B   C   D   E   F   G   H
+  * 110 111 112 113 114 115 116 117 118 119
+  * 
+  */
+  static mailbox64 = new Int8Array([
+    21, 22, 23, 24, 25, 26, 27, 28,
+    31, 32, 33, 34, 35, 36, 37, 38,
+    41, 42, 43, 44, 45, 46, 47, 48,
+    51, 52, 53, 54, 55, 56, 57, 58,
+    61, 62, 63, 64, 65, 66, 67, 68,
+    71, 72, 73, 74, 75, 76, 77, 78,
+    81, 82, 83, 84, 85, 86, 87, 88,
+    91, 92, 93, 94, 95, 96, 97, 98
+  ]);
+
+  static mailbox120 = new Int8Array([
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  ]);
+
+
+  /**
+   * PRINTS CURRENT BOARD TO TERMINAL
+   * can use decimal, character, or unicode notation
+   */
+  printBoard(pieceSymbol: "decimal" | "character" | "unicode" = "unicode") {
+    process.stdout.write(" +---------------------------+\n");
+    process.stdout.write("+-----------------------------+\n| |");
+
+    for (let i = 21; i <= 98; i++) {
+      if (i % 10 === 0) { process.stdout.write("| |"); continue; }
+      if (i % 10 === 9) { process.stdout.write(" | |\n"); continue; }
+
+      let square = "";
+      const piece = this.board[i] & 0b0001_0111;  // get *only* the piece and colour value for lookup
+
+      switch (pieceSymbol) {
+      case "decimal":
+        square = String(this.board[i]);
+        break;
+      case "character":
+        square = ` ${ChessBoard.PIECE_CH[piece as keyof typeof ChessBoard.PIECE_CH]} `;
+        break;
+      case "unicode":
+        square = ` ${ChessBoard.PIECE_UTF[piece as keyof typeof ChessBoard.PIECE_UTF]} `;
+        break;
+      default:
+        square = "ERR";
+        break;
+      }
+
+      square = square.padStart(3);
+      process.stdout.write(square);
+    }
+
+    process.stdout.write(" | |\n+-----------------------------+\n");
+    process.stdout.write(" +---------------------------+\n");
+  }
+
+
+  /**
+  * PADS A []64 BOARD WITH -1'S TO MAKE []120
+  */
+  static padBoard(inpBoard: Int8Array) {
+    const outBoard = new Int8Array([...ChessBoard.mailbox120]);
+    ChessBoard.mailbox64.forEach((mi, i) => {
+      outBoard[mi] = inpBoard[i];
+    });
+    return outBoard;
+  }
 }
--- src/engine.ts
+++ src/engine.ts
@@ -1,61 +1,111 @@
-import { MOVES_LIST, SLIDERS, SQ, mailbox64, padBoard } from "./board";
+import ChessBoard from "./board";
 
 /**
  * THE INITIAL CHESS POSITION
  */
 const INITIAL_BOARD = new Int8Array([
-  (SQ.b | SQ.R), (SQ.b | SQ.N), (SQ.b | SQ.B), (SQ.b | SQ.Q), (SQ.b | SQ.K | SQ.c), (SQ.b | SQ.B), (SQ.b | SQ.N), (SQ.b | SQ.R),
-  (SQ.b | SQ.P), (SQ.b | SQ.P), (SQ.b | SQ.P), (SQ.b | SQ.P), (SQ.b | SQ.P), (SQ.b | SQ.P), (SQ.b | SQ.P), (SQ.b | SQ.P),
+  20, 18, 19, 21, 54, 19, 18, 20,
+  17, 17, 17, 17, 17, 17, 17, 17,
   0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0,
-  (SQ.w | SQ.P), (SQ.w | SQ.P), (SQ.w | SQ.P), (SQ.w | SQ.P), (SQ.w | SQ.P), (SQ.w | SQ.P), (SQ.w | SQ.P), (SQ.w | SQ.P),
-  (SQ.w | SQ.R), (SQ.w | SQ.N), (SQ.w | SQ.B), (SQ.w | SQ.Q), (SQ.w | SQ.K | SQ.c), (SQ.w | SQ.B), (SQ.w | SQ.N), (SQ.w | SQ.R)
+  1, 1, 1, 1, 1, 1, 1, 1,
+  4, 2, 3, 5, 38, 3, 2, 4
 ]);
 
 
-
-export class Engine {
+export default class Engine {
+  chessboard: ChessBoard;
   board: Int8Array;
   currentTurn: number;
   enpassant: number;
 
   constructor() {
-    this.board = padBoard(INITIAL_BOARD);
-    this.currentTurn = SQ.w;
+    this.chessboard = new ChessBoard(INITIAL_BOARD);
+    this.board = this.chessboard.board;
+    this.currentTurn = ChessBoard.SQ.w;
     this.enpassant = 0;
   }
 
-  // pseudo-legal move generator
+  /**
+  * MOVE DIRECTIONS
+  * index matches piece encoding
+  * e.g. king = 6 = MOVES_LIST[6]
+  */
+  static NESW = {
+    N: -10,
+    S: 10,
+    E: 1,
+    W: -1,
+    NE: -9,
+    NW: -11,
+    SE: 11,
+    SW: 9,
+  };
+
+  /**
+  * THE POSSIBLE DIRECTIONS THAT EACH PIECE CAN GO IN 1-STEP
+  * can be accessed using the the piece value (excl. black pawn). i.e. knight = 2 = MOVES_LIST[2]
+  */
+  static MOVES_LIST = [
+    /* black pawn */[(Engine.NESW.S), (Engine.NESW.S + Engine.NESW.S), (Engine.NESW.SE), (Engine.NESW.SW)],
+    /* white pawn */[(Engine.NESW.N), (Engine.NESW.N + Engine.NESW.N), (Engine.NESW.NE), (Engine.NESW.NW)],
+    /*     knight */[(Engine.NESW.N + Engine.NESW.NE), (Engine.NESW.N + Engine.NESW.NW), (Engine.NESW.E + Engine.NESW.NE), (Engine.NESW.E + Engine.NESW.SE), (Engine.NESW.S + Engine.NESW.SE), (Engine.NESW.S + Engine.NESW.SW), (Engine.NESW.W + Engine.NESW.NW), (Engine.NESW.W + Engine.NESW.SW)],
+    /*     bishop */[(Engine.NESW.NE), (Engine.NESW.NW), (Engine.NESW.SE), (Engine.NESW.SW)],
+    /*       rook */[(Engine.NESW.N), (Engine.NESW.S), (Engine.NESW.E), (Engine.NESW.W)],
+    /*      queen */[(Engine.NESW.N), (Engine.NESW.S), (Engine.NESW.E), (Engine.NESW.W), (Engine.NESW.NE), (Engine.NESW.NW), (Engine.NESW.SE), (Engine.NESW.SW)],
+    /*       king */[(Engine.NESW.N), (Engine.NESW.S), (Engine.NESW.E), (Engine.NESW.W), (Engine.NESW.NE), (Engine.NESW.NW), (Engine.NESW.SE), (Engine.NESW.SW)]
+  ];
+
+
+  /**
+  * WHETHER A PIECE IS A SLIDER OR NOT (I.E. WHETHER IT CAN GO MORE THAN 1-STEP)
+  * can be accessed using the the piece value (excl. black pawn). i.e. knight = 2 = MOVES_LIST[2]
+  */
+  static SLIDERS = [
+    /* black pawn */ false,
+    /* white pawn */ false,
+    /*     knight */ false,
+    /*     bishop */ true,
+    /*       rook */ true,
+    /*      queen */ true,
+    /*       king */ false
+  ];
+
+
+  /**
+   * PSEUDO-LEGAL MOVE GENERATOR
+   */
   generateMoves() {
     for (let i = 0; i < 64; i++) {
-      const mb = mailbox64[i];  // get the mailbox index for the padded board
+      const mb = ChessBoard.mailbox64[i];  // get the mailbox index for the padded board
       const from = this.board[mb];
       const piece = from & 0b0000_0111; // check the piece type only
       const colour = from & 0b0001_0000;  // check the piece colour only
 
-      if (piece !== 0 && colour === this.currentTurn)
-        if (piece == SQ.P) {
+      if (piece !== 0 && colour === this.currentTurn) {
+        if (piece == ChessBoard.SQ.P) {
           // TODO: SPECIAL PAWN MOVE GENERATION
           continue;
         } else {
-          for (let j = 0; j < MOVES_LIST[piece].length; j++) { // iterate through the piece's possible move directions
-            const offset = MOVES_LIST[piece][j];  // the direction to move
+          for (let j = 0; j < Engine.MOVES_LIST[piece].length; j++) { // iterate through the piece's possible move directions
+            const offset = Engine.MOVES_LIST[piece][j];  // the direction to move
             while (true) { // infinite loop until a break is hit. i.e. edge of board, capture, etc.
               const to = this.board[mb + offset]; // the new square to move to
               if (to === -1) break; // off edge of board
               if ((to & 0b0000_0111) !== 0 && (to & 0b0001_0000) === this.currentTurn) break; // occupied by same colour piece
-
               // TODO: ADD MOVE TO STACK
-              if (SLIDERS[piece] === false) break;  // it shouldn't progress more than 1 step in any direction if it isn't a slider piece
+              if (Engine.SLIDERS[piece] === false) break;  // it shouldn't progress more than 1 step in any direction if it isn't a slider piece
             }
           }
         }
+      }
     }
   }
+  
 }
 
-
 const test = new Engine();
-test.generateMoves();
+test.chessboard.printBoard();
+// test.generateMoves();

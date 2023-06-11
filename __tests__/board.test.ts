97f5be3cacd06cec173b51559535b100a82fef40
marcobuontempo <marco.buon36@gmail.com>
2023-06-12 01:24:34 +1000

    Add mailbox array tests

--- __tests__/board.test.ts
+++ __tests__/board.test.ts
@@ -1,5 +1,17 @@
 import ChessBoard from "../src/board";
 
+describe("Mailbox Arrays", () => {
+  test("mailbox64 length", () => {
+    expect(ChessBoard.mailbox64.length).toStrictEqual(64);
+  });
+  test("mailbox120 length", () => {
+    expect(ChessBoard.mailbox120.length).toStrictEqual(120);
+  });
+  test("mailbox120 '-1' count", () => {
+    expect(ChessBoard.mailbox120.filter(i => i === -1).length).toStrictEqual(56);
+  });
+});
+
 describe("Unmoved Piece Values", () => {
   describe("white pieces", () => {
     test("white pawn should equal 1", () => {
@@ -97,89 +109,89 @@ describe("Moved Piece Values", () => {
 describe("Initialise ChessBoard", () => {
   test("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", () => {
     const chessboard = new ChessBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    expect(chessboard.castle).toStrictEqual(new Int8Array([1,1,1,1]));
+    expect(chessboard.castle).toStrictEqual(new Int8Array([1, 1, 1, 1]));
     expect(chessboard.turn).toStrictEqual(0);
     expect(chessboard.enpassant).toStrictEqual(-1);
     expect(chessboard.halfmove).toStrictEqual(0);
     expect(chessboard.fullmove).toStrictEqual(1);
     expect(chessboard.board).toStrictEqual(new Int8Array(
-      [ -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,20,82,83,85,54,83,82,20,-1,
-        -1,17,17,17,17,17,17,17,17,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,1,1,1,1,1,1,1,1,-1,
-        -1,4,66,67,69,38,67,66,4,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1]));
+      [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, 20, 82, 83, 85, 54, 83, 82, 20, -1,
+        -1, 17, 17, 17, 17, 17, 17, 17, 17, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 1, 1, 1, 1, 1, 1, 1, 1, -1,
+        -1, 4, 66, 67, 69, 38, 67, 66, 4, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]));
   });
 
   test("r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38", () => {
     const chessboard = new ChessBoard("r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38");
-    expect(chessboard.castle).toStrictEqual(new Int8Array([0,0,0,1]));
+    expect(chessboard.castle).toStrictEqual(new Int8Array([0, 0, 0, 1]));
     expect(chessboard.turn).toStrictEqual(16);
     expect(chessboard.enpassant).toStrictEqual(43);
     expect(chessboard.halfmove).toStrictEqual(0);
     expect(chessboard.fullmove).toStrictEqual(38);
     expect(chessboard.board).toStrictEqual(new Int8Array(
-      [ -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,20,0,0,0,54,0,0,0,-1,
-        -1,17,0,0,0,0,0,17,17,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,81,0,81,0,0,0,-1,
-        -1,0,0,0,0,65,0,0,0,-1,
-        -1,0,0,66,0,0,0,0,0,-1,
-        -1,0,0,0,1,0,1,1,1,-1,
-        -1,68,0,67,69,70,0,0,68,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1]));
+      [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, 20, 0, 0, 0, 54, 0, 0, 0, -1,
+        -1, 17, 0, 0, 0, 0, 0, 17, 17, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 81, 0, 81, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 65, 0, 0, 0, -1,
+        -1, 0, 0, 66, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 1, 0, 1, 1, 1, -1,
+        -1, 68, 0, 67, 69, 70, 0, 0, 68, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]));
   });
 
   test("8/8/4k3/8/8/3K4/8/8 w - - 32 120", () => {
     const chessboard = new ChessBoard("8/8/4k3/8/8/3K4/8/8 w - - 32 120");
-    expect(chessboard.castle).toStrictEqual(new Int8Array([0,0,0,0]));
+    expect(chessboard.castle).toStrictEqual(new Int8Array([0, 0, 0, 0]));
     expect(chessboard.turn).toStrictEqual(0);
     expect(chessboard.enpassant).toStrictEqual(-1);
     expect(chessboard.halfmove).toStrictEqual(32);
     expect(chessboard.fullmove).toStrictEqual(120);
     expect(chessboard.board).toStrictEqual(new Int8Array(
-      [ -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,86,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,70,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1]));
+      [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 86, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 70, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]));
   });
 
   test("7K/8/8/8/P7/8/8/k7 b a4 - 0 65", () => {
     const chessboard = new ChessBoard("7K/8/8/8/P7/8/8/k7 b - a4 0 65");
-    expect(chessboard.castle).toStrictEqual(new Int8Array([0,0,0,0]));
+    expect(chessboard.castle).toStrictEqual(new Int8Array([0, 0, 0, 0]));
     expect(chessboard.turn).toStrictEqual(16);
     expect(chessboard.enpassant).toStrictEqual(61);
     expect(chessboard.halfmove).toStrictEqual(0);
     expect(chessboard.fullmove).toStrictEqual(65);
     expect(chessboard.board).toStrictEqual(new Int8Array(
-      [ -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,0,0,0,0,0,0,0,70,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,65,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,0,0,0,0,0,0,0,0,-1,
-        -1,86,0,0,0,0,0,0,0,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1]));
+      [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 70, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 65, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, 86, 0, 0, 0, 0, 0, 0, 0, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]));
   });
 });

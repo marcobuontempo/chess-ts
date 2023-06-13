37c90cc2c4cd1901a949f1a5d2d795295e0fa559
marcobuontempo <marco.buon36@gmail.com>
2023-06-13 17:44:42 +1000

    Numbered test cases to more easily identify during debugging

--- __tests__/board.test.ts
+++ __tests__/board.test.ts
@@ -3,49 +3,49 @@ import ChessBoard from "../src/board";
 
 describe("Unmoved Piece Values", () => {
   describe("white pieces", () => {
-    test("white pawn should equal 1", () => {
+    test("1. white pawn should equal 1", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.P).toStrictEqual(1);
     });
-    test("white knight should equal 2", () => {
+    test("2. white knight should equal 2", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.N).toStrictEqual(2);
     });
-    test("white bishop should equal 3", () => {
+    test("3. white bishop should equal 3", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.B).toStrictEqual(3);
     });
-    test("white rook should equal 4", () => {
+    test("4. white rook should equal 4", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.R).toStrictEqual(4);
     });
-    test("white queen should equal 5", () => {
+    test("5. white queen should equal 5", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.Q).toStrictEqual(5);
     });
-    test("white king should equal 6", () => {
+    test("6. white king should equal 6", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.K).toStrictEqual(6);
     });
-    test("white king (with castle) should equal 38", () => {
+    test("7. white king (with castle) should equal 38", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.K | ChessBoard.SQ.c).toStrictEqual(38);
     });
   });
 
   describe("black pieces", () => {
-    test("black pawn should equal 17", () => {
+    test("1. black pawn should equal 17", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.P).toStrictEqual(17);
     });
-    test("black knight should equal 18", () => {
+    test("2. black knight should equal 18", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.N).toStrictEqual(18);
     });
-    test("black bishop should equal 19", () => {
+    test("3. black bishop should equal 19", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.B).toStrictEqual(19);
     });
-    test("black rook should equal 20", () => {
+    test("4. black rook should equal 20", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.R).toStrictEqual(20);
     });
-    test("black queen should equal 21", () => {
+    test("5. black queen should equal 21", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.Q).toStrictEqual(21);
     });
-    test("black king should equal 22", () => {
+    test("6. black king should equal 22", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.K).toStrictEqual(22);
     });
-    test("black king (with castle) should equal 54", () => {
+    test("7. black king (with castle) should equal 54", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.K | ChessBoard.SQ.c).toStrictEqual(54);
     });
   });
@@ -54,43 +54,43 @@ describe("Unmoved Piece Values", () => {
 
 describe("Moved Piece Values", () => {
   describe("white pieces", () => {
-    test("white pawn should equal 65", () => {
+    test("1. white pawn should equal 65", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.P | ChessBoard.SQ.m).toStrictEqual(65);
     });
-    test("white knight should equal 66", () => {
+    test("2. white knight should equal 66", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.N | ChessBoard.SQ.m).toStrictEqual(66);
     });
-    test("white bishop should equal 67", () => {
+    test("3. white bishop should equal 67", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.B | ChessBoard.SQ.m).toStrictEqual(67);
     });
-    test("white rook should equal 68", () => {
+    test("4. white rook should equal 68", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.R | ChessBoard.SQ.m).toStrictEqual(68);
     });
-    test("white queen should equal 69", () => {
+    test("5. white queen should equal 69", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.Q | ChessBoard.SQ.m).toStrictEqual(69);
     });
-    test("white king should equal 70", () => {
+    test("6. white king should equal 70", () => {
       expect(ChessBoard.SQ.w | ChessBoard.SQ.K | ChessBoard.SQ.m).toStrictEqual(70);
     });
   });
 
   describe("black pieces", () => {
-    test("black pawn should equal 81", () => {
+    test("1. black pawn should equal 81", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.P | ChessBoard.SQ.m).toStrictEqual(81);
     });
-    test("black knight should equal 82", () => {
+    test("2. black knight should equal 82", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.N | ChessBoard.SQ.m).toStrictEqual(82);
     });
-    test("black bishop should equal 83", () => {
+    test("3. black bishop should equal 83", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.B | ChessBoard.SQ.m).toStrictEqual(83);
     });
-    test("black rook should equal 84", () => {
+    test("4. black rook should equal 84", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.R | ChessBoard.SQ.m).toStrictEqual(84);
     });
-    test("black queen should equal 85", () => {
+    test("5. black queen should equal 85", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.Q | ChessBoard.SQ.m).toStrictEqual(85);
     });
-    test("black king should equal 86", () => {
+    test("6. black king should equal 86", () => {
       expect(ChessBoard.SQ.b | ChessBoard.SQ.K | ChessBoard.SQ.m).toStrictEqual(86);
     });
   });
@@ -111,7 +111,7 @@ describe("Mailbox Arrays", () => {
 
 
 describe("Parse FEN", () => {
-  test("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", () => {
+  test("1. rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", () => {
     const chessboard = new ChessBoard();
     const chessboardProto = Object.getPrototypeOf(chessboard);
     expect(chessboardProto.constructor.parseFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")).toStrictEqual(
@@ -141,7 +141,7 @@ describe("Parse FEN", () => {
 
 
 describe("Pad Board with -1's", () => {
-  test("8x8 blank array", () => {
+  test("1. 8x8 blank array", () => {
     const board = new Int8Array([
       0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 0,
@@ -173,7 +173,7 @@ describe("Pad Board with -1's", () => {
 
 
 describe("Initialise ChessBoard", () => {
-  test("No FEN provided in constructor", () => {
+  test("1. No FEN provided in constructor", () => {
     const chessboard = new ChessBoard();
     expect(chessboard.castle).toStrictEqual(new Int8Array([1, 1, 1, 1]));
     expect(chessboard.turn).toStrictEqual(0);
@@ -194,7 +194,7 @@ describe("Initialise ChessBoard", () => {
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]));
   });
-  test("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", () => {
+  test("2. rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", () => {
     const chessboard = new ChessBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     expect(chessboard.castle).toStrictEqual(new Int8Array([1, 1, 1, 1]));
     expect(chessboard.turn).toStrictEqual(0);
@@ -215,7 +215,7 @@ describe("Initialise ChessBoard", () => {
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]));
   });
-  test("r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38", () => {
+  test("3. r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38", () => {
     const chessboard = new ChessBoard("r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38");
     expect(chessboard.castle).toStrictEqual(new Int8Array([0, 0, 0, 1]));
     expect(chessboard.turn).toStrictEqual(16);
@@ -236,7 +236,7 @@ describe("Initialise ChessBoard", () => {
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]));
   });
-  test("8/8/4k3/8/8/3K4/8/8 w - - 32 120", () => {
+  test("4. 8/8/4k3/8/8/3K4/8/8 w - - 32 120", () => {
     const chessboard = new ChessBoard("8/8/4k3/8/8/3K4/8/8 w - - 32 120");
     expect(chessboard.castle).toStrictEqual(new Int8Array([0, 0, 0, 0]));
     expect(chessboard.turn).toStrictEqual(0);
@@ -257,7 +257,7 @@ describe("Initialise ChessBoard", () => {
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]));
   });
-  test("7K/8/8/8/P7/8/8/k7 b - a4 0 65", () => {
+  test("5. 7K/8/8/8/P7/8/8/k7 b - a4 0 65", () => {
     const chessboard = new ChessBoard("7K/8/8/8/P7/8/8/k7 b - a4 0 65");
     expect(chessboard.castle).toStrictEqual(new Int8Array([0, 0, 0, 0]));
     expect(chessboard.turn).toStrictEqual(16);
@@ -282,23 +282,23 @@ describe("Initialise ChessBoard", () => {
 
 
 describe("Get FEN", () => {
-  test("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", () => {
+  test("1. rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", () => {
     const chessboard = new ChessBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     expect(chessboard.getFEN()).toStrictEqual("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
   });
-  test("r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38", () => {
+  test("2. r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38", () => {
     const chessboard = new ChessBoard("r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38");
     expect(chessboard.getFEN()).toStrictEqual("r3k3/p5pp/8/2p1p3/4P3/2N5/3P1PPP/R1BQK2R b q c6 0 38");
   });
-  test("8/8/4k3/8/8/3K4/8/8 w - - 32 120", () => {
+  test("3. 8/8/4k3/8/8/3K4/8/8 w - - 32 120", () => {
     const chessboard = new ChessBoard("8/8/4k3/8/8/3K4/8/8 w - - 32 120");
     expect(chessboard.getFEN()).toStrictEqual("8/8/4k3/8/8/3K4/8/8 w - - 32 120");
   });
-  test("7K/8/8/8/P7/8/8/k7 b - a4 0 65", () => {
+  test("4. 7K/8/8/8/P7/8/8/k7 b - a4 0 65", () => {
     const chessboard = new ChessBoard("7K/8/8/8/P7/8/8/k7 b - a4 0 65");
     expect(chessboard.getFEN()).toStrictEqual("7K/8/8/8/P7/8/8/k7 b - a4 0 65");
   });
-  test("8/8/8/8/8/8/8/8 w - - 5 10", () => {
+  test("5. 8/8/8/8/8/8/8/8 w - - 5 10", () => {
     const chessboard = new ChessBoard();
     chessboard.board = new Int8Array([
       -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -324,23 +324,23 @@ describe("Get FEN", () => {
 
 
 describe("Encode Square", () => {
-  test("white pawn unmoved", () => {
+  test("1. white pawn unmoved", () => {
     expect(ChessBoard.encodeSquare("P", false, false)).toStrictEqual(1);
   });
-  test("black rook moved", () => {
+  test("2. black rook moved", () => {
     expect(ChessBoard.encodeSquare("r", true, false)).toStrictEqual(84);
   });
-  test("white king moved", () => {
+  test("3. white king moved", () => {
     expect(ChessBoard.encodeSquare("K", true, false)).toStrictEqual(70);
   });
-  test("black king unmoved (can castle)", () => {
+  test("4. black king unmoved (can castle)", () => {
     expect(ChessBoard.encodeSquare("k", false, true)).toStrictEqual(54);
   });
 });
 
 
 describe("Decode Square", () => {
-  test("white pawn unmoved", () => {
+  test("1. white pawn unmoved", () => {
     const square = ChessBoard.encodeSquare("P",false,false);
     expect(ChessBoard.decodeSquare(square)).toStrictEqual({
       piece: "P",
@@ -348,21 +348,21 @@ describe("Decode Square", () => {
       canCastle: false,
     });
   });
-  test("black rook moved", () => {
+  test("2. black rook moved", () => {
     const square = ChessBoard.encodeSquare("r",true,false);
     expect(ChessBoard.decodeSquare(square)).toStrictEqual({
       piece: "r",
       hasMoved: true,
       canCastle: false,
     });  });
-  test("white king moved", () => {
+  test("3. white king moved", () => {
     const square = ChessBoard.encodeSquare("K",true,false);
     expect(ChessBoard.decodeSquare(square)).toStrictEqual({
       piece: "K",
       hasMoved: true,
       canCastle: false,
     });  });
-  test("black king unmoved (can castle)", () => {
+  test("4. black king unmoved (can castle)", () => {
     const square = ChessBoard.encodeSquare("k",false,true);
     expect(ChessBoard.decodeSquare(square)).toStrictEqual({
       piece: "k",
--- __tests__/engine.test.ts
+++ __tests__/engine.test.ts
@@ -2,26 +2,26 @@ import ChessBoard from "../src/board";
 import Engine from "../src/engine";
 
 describe("Encode Move Data", () => {
-  test("Castle[No], Capture[No], Promotion[No], From[21], To[22]", () => {
+  test("1. Castle[No], Capture[No], Promotion[No], From[21], To[22]", () => {
     expect(Engine.encodeMoveData(0,0,0,21,22)).toStrictEqual(0b0000_0000_0000_0000_0001_0101_0001_0110);
   });
-  test("Castle[No], Capture[Rook], Promotion[No], From[98], To[28]", () => {
+  test("2. Castle[No], Capture[Rook], Promotion[No], From[98], To[28]", () => {
     expect(Engine.encodeMoveData(0,ChessBoard.SQ.R,0,98,28)).toStrictEqual(0b0000_0000_0010_0000_0110_0010_0001_1100);
   });
-  test("Castle[QueenSide], Capture[No], Promotion[No], From[25], To[21]", () => {
+  test("3. Castle[QueenSide], Capture[No], Promotion[No], From[25], To[21]", () => {
     expect(Engine.encodeMoveData(2,0,0,25,21)).toStrictEqual(0b0000_0000_1000_0000_0001_1001_0001_0101);
   });
-  test("Castle[No], Capture[Knight], Promotion[Queen], From[87], To[96]", () => {
+  test("4. Castle[No], Capture[Knight], Promotion[Queen], From[87], To[96]", () => {
     expect(Engine.encodeMoveData(0,ChessBoard.SQ.N,ChessBoard.SQ.Q,87,96)).toStrictEqual(0b0000_0000_0001_0101_0101_0111_0110_0000);
   });
-  test("Castle[KingSide], Capture[No], Promotion[No], From[95], To[98]", () => {
+  test("5. Castle[KingSide], Capture[No], Promotion[No], From[95], To[98]", () => {
     expect(Engine.encodeMoveData(1,0,0,95,98)).toStrictEqual(0b0000_0000_0100_0000_0101_1111_0110_0010);
   });
 });
 
 // decode move data
 describe("Encode Move Data", () => {
-  test("Castle[No], Capture[No], Promotion[No], From[21], To[22]", () => {
+  test("1. Castle[No], Capture[No], Promotion[No], From[21], To[22]", () => {
     expect(Engine.decodeMoveData(0b0000_0000_0000_0000_0001_0101_0001_0110)).toStrictEqual({
       castle: 0,
       capture: 0,
@@ -30,7 +30,7 @@ describe("Encode Move Data", () => {
       to: 22,
     });
   });
-  test("Castle[No], Capture[Rook], Promotion[No], From[98], To[28]", () => {
+  test("2. Castle[No], Capture[Rook], Promotion[No], From[98], To[28]", () => {
     expect(Engine.decodeMoveData(0b0000_0000_0010_0000_0110_0010_0001_1100)).toStrictEqual({
       castle: 0,
       capture: ChessBoard.SQ.R,
@@ -39,7 +39,7 @@ describe("Encode Move Data", () => {
       to: 28,
     });
   });
-  test("Castle[QueenSide], Capture[No], Promotion[No], From[25], To[21]", () => {
+  test("3. Castle[QueenSide], Capture[No], Promotion[No], From[25], To[21]", () => {
     expect(Engine.decodeMoveData(0b0000_0000_1000_0000_0001_1001_0001_0101)).toStrictEqual({
       castle: 2,
       capture: 0,
@@ -48,7 +48,7 @@ describe("Encode Move Data", () => {
       to: 21,
     });
   });
-  test("Castle[No], Capture[Knight], Promotion[Queen], From[87], To[96]", () => {
+  test("4. Castle[No], Capture[Knight], Promotion[Queen], From[87], To[96]", () => {
     expect(Engine.decodeMoveData(0b0000_0000_0001_0101_0101_0111_0110_0000)).toStrictEqual({
       castle: 0,
       capture: ChessBoard.SQ.N,
@@ -57,7 +57,7 @@ describe("Encode Move Data", () => {
       to: 96,
     });
   });
-  test("Castle[KingSide], Capture[No], Promotion[No], From[95], To[98]", () => {
+  test("5. Castle[KingSide], Capture[No], Promotion[No], From[95], To[98]", () => {
     expect(Engine.decodeMoveData(0b0000_0000_0100_0000_0101_1111_0110_0010)).toStrictEqual({
       castle: 1,
       capture: 0,
